3342634becd3adc0e011532ca12ce09961aa3757
Mykola Morgunenko <Olli25010@gmail.com>
2020-04-10 22:55:46 +0300

    add myralllka test

--- myralllka_test/task.py
+++ myralllka_test/task.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 
 
 # Task
-# So, usualy when you do an operation with a non-stationary vehicle ( like UAV,
+# So, usualy when you do an operation with a non-stationary vehicle ( like
+# UAV,
 # copter ) like following, or idling near an sensor observed object to avoid
 # and prohibit any oscilation of the vehicle due to false-positive detection,
 # sensor noise, etc ( lot's of problem occurs when you go out from your
@@ -32,30 +33,43 @@ import matplotlib.pyplot as plt
 #    - new_ref - this one that should be executed by our vehicle.
 # Function check_reference. The intention is to check whether  the input
 # refence is in or out from our deadband factor.
-#   For this, you need to project the reference into the subspace between
-#   target and drone. The norm of this projection will be our deadBand factor.
-#   This factor will decide if we are changing the reference or not. (is it in
-#   deadBand region or not )
+#   For this, you need to
+#   TODO: project the reference into the subspace between target and drone.
+#    The norm of this projection will be our deadBand factor.
+#   TODO: This factor will decide if we are changing the reference or not. (
+#    is it in deadBand region or not )
 #   If we are updating the reference, we should project the reference to the
 #   drone's plane. The goal is not move the drone ( aka vehicle ) closer or
 #   further from the object is the factor is not big enough, we should only
 #   move it in it's plane.
 
-def check_reference(target, uav_odom, reference, factor):
-    # TODO: implement Dead Band contorl
+def check_reference(target, uav_odometry, reference, factor):
+    subspace = np.subtract(target, uav_odometry)
+    projection_matrix = np.true_divide(np.outer(subspace, subspace),
+                                       np.dot(subspace, subspace))
+    res = np.matmul(projection_matrix, np.subtract(reference,
+                                                   uav_odometry))
+    res = np.add(res, uav_odometry)
+    # if we wont to update on target
+    reference = target if (np.dot(reference, res) > factor) else reference
+    # or update to a projection
+    # reference = res if (np.dot(reference, res) > factor) else reference
     return reference  # return the updated or not updated reference
 
 
 target = np.array([10, 10, 0])
 
-uav_odom = np.array([10, 1, 0])
+uav_odom = np.array([5, 1, 0])
 
-original_reference = np.array([10, 8, 0])
+original_reference = np.array([12, 7, 0])
 
-deadBand = 3  # [m]
+deadBand = 1  # [m]
 
 new_ref = check_reference(target, uav_odom, original_reference, deadBand)
-
+# plt.xticks(np.arange(0, 2, step=0.2))
+# plt.yticks(np.arange(0, 2, step=0.2))yscale(
+# plt.yscale('linear')
+# plt.xscale('linear')
 # plot the target red
 plt.plot(target[0], target[1], 'ro', label='target')
 # plot the uav blue
@@ -67,6 +81,6 @@ plt.plot(original_reference[0], original_reference[1], 'bx',
 
 # plot the new reference
 plt.plot(new_ref[0], new_ref[1], 'go', label='new reference')
-
 plt.legend(loc="upper left")
+plt.axis('equal')
 plt.show()
